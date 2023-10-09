4f41928d5114643f132dbb571ee7b7264fea01a4
David A. Adams <david.adams97@gmail.com>
2023-10-09 16:03:47 -0400

    feat: initial commit

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,39 +1,11 @@
-<script setup>
-defineProps({});
-</script>
-
 <template>
-    <div class="topnav">
-        <a class="active" href="#home">Home</a>
-    </div>
+    <button>{{ buttonText.connectToSpotify }}</button>
 </template>
 
-<style>
-/* Add a black background color to the top navigation */
-.topnav {
-    background-color: #333;
-    overflow: hidden;
-}
+<script setup>
 
-/* Style the links inside the navigation bar */
-.topnav a {
-    float: left;
-    color: #f2f2f2;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
+const buttonText = {
+    connectToSpotify: "Connect to Spotify"
 }
+</script>
 
-/* Change the color of links on hover */
-.topnav a:hover {
-    background-color: #ddd;
-    color: black;
-}
-
-/* Add a color to the active/current link */
-.topnav a.active {
-    background-color: #1ca6d4;
-    color: white;
-}
-</style>
