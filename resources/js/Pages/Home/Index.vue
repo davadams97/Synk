44ad31d36cc0d3b4c06126f792ec04fde858a9f2
David A. Adams <david.adams97@gmail.com>
2023-11-07 01:12:17 -0500

    feat: use link element

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -1,14 +1,19 @@
 <template>
     <div class="flex bg-sky-950 h-screen items-center justify-center">
-        <button
+        <Link
             class="flex text-slate-50 w-50 h-12 m-2 p-4 items-center rounded-md bg-slate-600"
+            as="button"
+            type="button"
+            :href="route('spotify.authorize')"
         >
             {{ buttonText.connectToSpotify }}
-        </button>
+        </Link>
     </div>
 </template>
 
 <script setup>
+import { Link } from '@inertiajs/vue3'
+
 const buttonText = {
     connectToSpotify: "Connect to Spotify",
 };
