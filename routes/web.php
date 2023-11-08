959e0571c23e04e60e7dd7c7787ad893f6ef39b1
David A. Adams <david.adams97@gmail.com>
2023-11-08 00:12:51 -0500

    feat: add routes access token

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,15 @@
 use App\Http\Controllers\HomePageController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
+/** Inertia Routes */
 Route::get('/', [HomePageController::class, 'index']);
 
+/** Spotify API Routes */
 Route::get('/redirect', function (Request $request) {
     $request->session()->put('state', $state = Str::random(40));
 
@@ -24,7 +27,7 @@ Route::get('/redirect', function (Request $request) {
 
     $query = http_build_query([
         'client_id' => env('SPOTIFY_CLIENT_ID'),
-        'redirect_uri' => 'http://localhost:8000',
+        'redirect_uri' => 'http://localhost:8000/access-token',
         'response_type' => 'code',
         'scope' => 'user-read-private user-read-email',
         'state' => $state,
@@ -35,3 +38,22 @@ Route::get('/redirect', function (Request $request) {
     return redirect('https://accounts.spotify.com/authorize?' . $query);
 })->name('spotify.authorize');
 
+Route::get('/access-token', function (Request $request) {
+    $state = $request->session()->pull('state');
+    $codeVerifier = $request->session()->pull('code_verifier');
+ 
+    throw_unless(
+        strlen($state) > 0 && $state === $request->state,
+        InvalidArgumentException::class
+    );
+ 
+    $response = Http::asForm()->post('https://accounts.spotify.com/api/token', [
+        'grant_type' => 'authorization_code',
+        'client_id' => env('SPOTIFY_CLIENT_ID'),
+        'redirect_uri' => 'http://localhost:8000/access-token',
+        'code_verifier' => $codeVerifier,
+        'code' => $request->code,
+    ]);
+ 
+    return $response->json();
+})->name('spotify.accessToken');
