d4268c513151da7621dfa243143fcd3d2d708e85
David A. Adams <david.adams97@gmail.com>
2023-11-13 23:43:15 -0500

    feat: get user playlist

--- app/Http/Controllers/SpotifyController.php
+++ app/Http/Controllers/SpotifyController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
 use Inertia\Response;
 
 class SpotifyController extends Controller
 {
     public function index(): Response
     {
-        return inertia('Spotify/Index');
+        $userName = $this->getProfile()["display_name"];
+        $playlists = array_map(fn ($playlist) => $playlist['name'], $this->getPlaylists());
+
+        return inertia('Spotify/Index', [
+            'userName' => $userName,
+            'playlists' => $playlists
+        ]);
+    }
+
+    public function getProfile()
+    {
+        return Http::withToken(session('spotifyAccessToken'))->get('https://api.spotify.com/v1/me')->json();
+    }
+
+    public function getPlaylists()
+    {
+        return Http::withToken(session('spotifyAccessToken'))->get('https://api.spotify.com/v1/me/playlists')['items'];
     }
 }
