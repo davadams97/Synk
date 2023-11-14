17e8a749bd051643987d3be753151f4854cbb3ae
David A. Adams <david.adams97@gmail.com>
2023-11-13 23:43:47 -0500

    chore: render playlists

--- resources/js/Pages/Spotify/Index.vue
+++ resources/js/Pages/Spotify/Index.vue
@@ -1,9 +1,21 @@
 <template>
     <div class="flex bg-sky-950 h-screen justify-center">
-        <h2 class="text-slate-50 w-50 h-12 m-2 p-4">
-            Showing your current playlist for Spotify
-        </h2>
+        <section>
+            <h2 class="text-slate-50 w-50 h-12 m-2 p-4">
+                Showing your current playlist for Spotify
+            </h2>
+            <div>
+                <span class="text-slate-50 w-50 h-12 m-2 p-4"
+                    >Showing Spotify playlist for {{ userName }}</span
+                >
+                <ul v-for="playlist of playlists">
+                    <li>{{ playlist }}</li>
+                </ul>
+            </div>
+        </section>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+const { userName } = defineProps(["userName", "playlists"]);
+</script>
