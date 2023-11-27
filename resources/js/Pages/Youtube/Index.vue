b60ca1f80e544d5b1668d96d5866144a4956b0ba
David A. Adams <david.adams97@gmail.com>
2023-11-27 14:22:08 -0500

    feat: use table

--- resources/js/Pages/Youtube/Index.vue
+++ resources/js/Pages/Youtube/Index.vue
@@ -4,18 +4,22 @@
             <h2 class="text-slate-50 w-50 h-12 m-2 p-4">
                 Showing your current playlist for Youtube
             </h2>
-            <div>
-                <span class="text-slate-50 w-50 h-12 m-2 p-4"
-                    >Showing Youtube playlist for {{ userName }}</span
-                >
-                <ul v-for="playlist of playlists">
-                    <li>{{ playlist.title }}</li>
-                </ul>
-            </div>
+
+            <table class="text-slate-50">
+                <tr>
+                    <th>Name of playlist</th>
+                </tr>
+                <tr v-for="playlist of playlists">
+                    <td>
+                        <Link href="/">{{ playlist.title }}</Link>
+                    </td>
+                </tr>
+            </table>
         </section>
     </div>
 </template>
 
 <script setup>
+import { Link } from "@inertiajs/vue3";
 defineProps(["userName", "playlists"]);
 </script>
