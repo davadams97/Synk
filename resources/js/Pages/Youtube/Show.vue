99493bc5117febc8f0bdde10c5ed95f628285b9a
David A. Adams <david.adams97@gmail.com>
2023-11-27 15:18:15 -0500

    feat: display track info

---
+++ resources/js/Pages/Youtube/Show.vue
@@ -0,0 +1,18 @@
+<template>
+    Showing playlist data
+    <table>
+        <tr>
+            <th>Song title</th>
+            <th>Album</th>
+        </tr>
+        <tr v-for="track of playlist">
+            <td>
+                {{ track.name }}
+            </td>
+            <td>{{ track.album }}</td>
+        </tr>
+    </table>
+</template>
+<script setup>
+defineProps(["playlist"]);
+</script>
