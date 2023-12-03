87952a00171b20766e5ada6e237d292b9d80d801
David A. Adams <david.adams97@gmail.com>
2023-12-03 14:16:12 -0500

    style: remove flex

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="flex bg-sky-950 h-screen px-4 py-8 md:flex-1 md:p-12 md:overflow-y-auto">
+    <div
+        class="bg-sky-950 h-screen px-4 py-8 md:flex-1 md:p-12 md:overflow-y-auto"
+    >
         <slot></slot>
     </div>
 </template>
