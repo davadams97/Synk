c5efd68a2030ee5aec99f9ca3c0169a10f055fe2
David A. Adams <david.adams97@gmail.com>
2023-12-02 19:23:58 -0500

    style: update styling

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex bg-sky-950 h-screen">
+    <div class="flex bg-sky-950 h-screen px-4 py-8 md:flex-1 md:p-12 md:overflow-y-auto">
         <slot></slot>
     </div>
 </template>
