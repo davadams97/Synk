20a00402bc8005c8d7f53b0e73a5823936fd0b62
David A. Adams <david.adams97@gmail.com>
2023-12-05 17:53:43 -0500

    chore: add placeholder button

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -7,10 +7,22 @@
         >
             {{ button.label }}
         </a>
+
+        <Link
+            class="flex text-slate-50 w-50 h-12 m-2 p-4 items-center rounded-md bg-slate-600"
+            :href="tranferButtonConfig.href"
+            method="post"
+            as="button"
+            :data="{ title: 'Test Playlist' }"
+        >
+            {{ tranferButtonConfig.label }}
+        </Link>
     </div>
 </template>
 
 <script setup>
+import { Link } from "@inertiajs/vue3";
+
 const buttonConfig = [
     {
         providerName: "Spotify",
@@ -23,4 +35,10 @@ const buttonConfig = [
         href: route("youtube.playlist"),
     },
 ];
+
+const tranferButtonConfig = {
+    providerName: "Youtube Music",
+    label: "Transfer Playlist",
+    href: route("transfer.playlist"),
+};
 </script>
