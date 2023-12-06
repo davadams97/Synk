d417f537397511d430bd3e3edd53569dbac5968c
David A. Adams <david.adams97@gmail.com>
2023-12-06 14:44:54 -0500

    feat: initial iteration for adding song to yt playlist

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -9,8 +9,43 @@ class TransferController extends Controller
 {
     public function store(Request $request)
     {
-        Http::withHeaders(['Content-Type' => 'application/json'])->post(env('YOUTUBE_API') . 'playlists', [
-            'title' => $request['title']
+        $playlistId = $this->createPlaylist($request['title']);
+        $result = $this->findSongFromYTMusic($request['name']);
+        $this->addSongToPlaylist($result['videoId'], $playlistId);
+
+        // TODO: check for explicit
+        // TODO: check for casing 
+        // TODO: check for song name and artist
+    }
+
+    private function createPlaylist($title)
+    {
+        return Http::withHeaders(['Content-Type' => 'application/json'])->post(env('YOUTUBE_API').'playlists', [
+            'title' => $title,
+        ]);
+
+        // Handle error
+    }
+
+    private function findSongFromYTMusic($songName)
+    {
+        $searchResults = Http::get(env('YOUTUBE_API').'search', [
+            'query' => $songName,
+        ])->json();
+
+        $matchedResult = array_filter(
+            $searchResults,
+            fn ($result) => strtolower($result['title']) === strtolower($songName)
+        )[0];
+
+        return $matchedResult;
+        // Handle error
+    }
+
+    private function addSongToPlaylist($videoId, $playlistId)
+    {
+        Http::withHeaders(['Content-Type' => 'application/json'])->post(env('YOUTUBE_API').'playlists/'.$playlistId.'/add-songs', [
+            'videoIds' => [$videoId],
         ]);
     }
 }
