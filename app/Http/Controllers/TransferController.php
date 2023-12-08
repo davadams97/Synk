a2ff1c38f1c2df4a55c46988447be3f3279a5f55
David A. Adams <david.adams97@gmail.com>
2023-12-07 23:54:15 -0500

    feat: add fuzzy search 1st iteration

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Fuse\Fuse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -9,12 +10,29 @@ class TransferController extends Controller
 {
     public function store(Request $request)
     {
+        $songsToAdd = [];
+
+        foreach ($request['name'] as $song) {
+            $result = $this->findSongFromYTMusic($song);
+            // don't do anything if result is empty
+            $songsToAdd[] = $result[0]['item'];
+        }
+
         $playlistId = $this->createPlaylist($request['title']);
-        $result = $this->findSongFromYTMusic($request['name']);
-        $this->addSongToPlaylist($result['videoId'], $playlistId);
+
+        $songsToAddIds = array_map(fn ($song) => $song['videoId'], $songsToAdd);
+
+        info($songsToAddIds);
+
+        // foreach ($songsToAdd as $song) {
+        // info($song);
+        $this->addSongToPlaylist($songsToAddIds, $playlistId);
+        // }
+
+        // delete playlist if fail
 
         // TODO: check for explicit
-        // TODO: check for casing 
+        // TODO: check for casing
         // TODO: check for song name and artist
     }
 
@@ -29,23 +47,35 @@ class TransferController extends Controller
 
     private function findSongFromYTMusic($songName)
     {
+        $filter = [
+            'keys' =>
+            // ['name' => 'title', 'getFn' => fn ($res) => $res['title']],
+            // ['name' => 'album', 'getFn' => fn ($res) => $res['album']['name']],
+            ['title', 'artists.name', 'album.name'],
+            'shouldSort' => true,
+        ];
+
         $searchResults = Http::get(env('YOUTUBE_API').'search', [
             'query' => $songName,
         ])->json();
 
-        $matchedResult = array_filter(
-            $searchResults,
-            fn ($result) => strtolower($result['title']) === strtolower($songName)
-        )[0];
+        $fuse = new Fuse($searchResults, $filter);
+
+        $matchedResult = $fuse->search($songName, ['limit' => 1]);
+
+        // $matchedResult = array_filter(
+        //     $searchResults,
+        //     fn ($result) => strtolower($result['title']) === strtolower($songName)
+        // )[0];
 
         return $matchedResult;
         // Handle error
     }
 
-    private function addSongToPlaylist($videoId, $playlistId)
+    private function addSongToPlaylist($videoIds, $playlistId)
     {
         Http::withHeaders(['Content-Type' => 'application/json'])->post(env('YOUTUBE_API').'playlists/'.$playlistId.'/add-songs', [
-            'videoIds' => [$videoId],
+            'videoIds' => $videoIds,
         ]);
     }
 }
