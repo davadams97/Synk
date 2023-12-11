efffadff6e61cae6999390c7ee24ea4b0f95e61d
David A. Adams <david.adams97@gmail.com>
2023-12-11 13:44:23 -0500

    feat: add methods

--- app/Services/YoutubeMusicService.php
+++ app/Services/YoutubeMusicService.php
@@ -20,4 +20,30 @@ class YoutubeMusicService
     {
         return Http::get(env('YOUTUBE_API').'playlists/'.$playlistId)->json();
     }
+
+    public function createPlaylist(string $title)
+    {
+        return Http::withHeaders(['Content-Type' => 'application/json'])->post(env('YOUTUBE_API').'playlists', [
+            'title' => $title,
+        ]);
+    }
+
+    public function deletePlaylist(string $playlistId)
+    {
+        return Http::delete(env('YOUTUBE_API').'playlists/'.$playlistId);
+    }
+
+    public function addToPlaylist(string $playlistId, $trackIds)
+    {
+        return Http::withHeaders(['Content-Type' => 'application/json'])->post(env('YOUTUBE_API').'playlists/'.$playlistId.'/add-songs', [
+            'videoIds' => $trackIds,
+        ]);
+    }
+
+    public function searchTracks($query)
+    {
+        return Http::get(env('YOUTUBE_API').'search', [
+            'query' => $query,
+        ]);
+    }
 }
