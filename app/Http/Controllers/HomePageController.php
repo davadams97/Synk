067c5db54d6f8748fa4eb1eb57d5ea0e7c251326
David A. Adams <david.adams97@gmail.com>
2023-12-16 16:34:27 -0500

    feat: pass button config from server

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -8,6 +8,22 @@ class HomePageController extends Controller
 {
     public function index(): Response
     {
-        return inertia('Home/Index');
+        $buttonConfig = [
+            [
+                'providerName' => 'Spotify',
+                'label' => session('spotifyAccessToken') ? 'View Spotify Playlist' : 'Connect to Spotify',
+                'href' => session('spotifyAccessToken') ? route('spotify.playlist') : route('spotify.authorize'),
+            ],
+            [
+                'providerName' => 'Youtube Music',
+                'label' => 'Connect to Youtube Music',
+                'href' => route('youtube.playlist'),
+            ],
+        ];
+
+        return inertia('Home/Index',
+            [
+                'buttonConfig' => $buttonConfig,
+            ]);
     }
 }
