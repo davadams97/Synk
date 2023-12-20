c3f8c8bd56531fa5f6af1c34d130ad2f89b4753d
David A. Adams <david.adams97@gmail.com>
2023-12-20 18:24:29 -0500

    style: update styles and text

--- resources/js/Pages/Home/Show.vue
+++ resources/js/Pages/Home/Show.vue
@@ -1,23 +1,22 @@
 <template>
-    <div class="flex">
-        <h1 class="mb-4 text-4xl font-bold leading-none tracking-tight md:text-5xl lg:text-6xl text-white">
-            Your Playlist, Your Rules, Any Platform!
+    <div class="flex items-center p-10">
+        <h1 class="flex flex-col mb-4 text-right text-4xl font-bold leading-none tracking-tight md:text-5xl lg:text-6xl text-slate-200">
+            <span class="pb-2">Your Playlist</span>
+            <span class="pb-2"> Your Rules</span>
+            <span class="pb-2">Any Platform!</span>
         </h1>
 
         <div class="flex flex-col">
 
-            <p class="mb-6 text-lg font-normal text-gray-400 lg:text-xl sm:px-16 xl:px-48">
-                Elevate your music experience with Synk, your go-to app
-                for seamless playlist transfers. Whether you're grooving on Spotify,
-                vibing on Apple Music, or jamming on another platform,
-                Synk makes it a breeze to keep your tunes in tune.
-                Explore musical freedom and sync the beat your way—because your
-                playlist deserves to dance across every platform effortlessly.
-                Unleash the power of Synk and take your music wherever life's rhythm takes you!
+            <p class="mb-6 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48">
+                Sync your tunes effortlessly across platforms with Synk. Groove on Spotify,
+                vibe on Apple Music, or jam on any platform – Synk makes it easy.
+                Your playlist deserves to dance everywhere, effortlessly. Unleash the power of
+                Synk and take your music anywhere life's rhythm takes you!
             </p>
 
             <a class="inline-flex items-center px-5 py-3 text-base max-w-fit self-center
-                      font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800
+                      font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800
                       focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" href="">
                 Let's get started
                 <svg aria-hidden="true" class="w-3.5 h-3.5 ms-2 rtl:rotate-180" fill="none" viewBox="0 0 14 10"
