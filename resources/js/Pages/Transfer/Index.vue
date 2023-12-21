53b5d8a56247cf6c073a1539635493ccb0f1829e
David A. Adams <david.adams97@gmail.com>
2023-12-20 23:41:05 -0500

    style: load icons in a row format

--- resources/js/Pages/Transfer/Index.vue
+++ resources/js/Pages/Transfer/Index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="w-1/2 bg-gray-700/50 rounded-2xl p-4 m-8">
-        <div class="flex h-full items-center justify-center">
+        <div class="flex flex-row h-full items-start">
             <a
                 v-for="button in buttonConfig"
                 class="flex relative text-slate-50 w-50 h-50 m-4 p-4 hover:bg-slate-700 focus:ring-4 focus:ring-blue-300
-                 items-center rounded-md bg-slate-600/40"
+                 items-center rounded-md"
                 :href="button.href"
             >
                 <img :src="button.logo" :alt="button.alt">
@@ -20,11 +20,11 @@
     <span class="flex items-center scale-150 text-white">&#8594;</span>
 
     <div class="w-1/2 bg-gray-700/50 rounded-2xl p-4 m-8">
-        <div class="flex h-full items-center justify-center">
+        <div class="flex flex-row h-full items-start">
             <a
                 v-for="button in buttonConfig"
                 class="flex relative text-slate-50 w-50 h-50 m-4 p-4 hover:bg-slate-700 focus:ring-4 focus:ring-blue-300
-                 items-center rounded-md bg-slate-600/40"
+                 items-center rounded-md"
                 :href="button.href"
             >
                 <img :src="button.logo" :alt="button.alt">
