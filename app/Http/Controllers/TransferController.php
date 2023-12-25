c7dc43aad35c2884090aabb65410eee360df7d66
David A. Adams <david@stemble.ca>
2023-12-24 19:03:47 -0500

    feat: update button configutaion

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Response;
 
@@ -11,22 +12,52 @@ class TransferController extends Controller
     {
         $buttonConfig = [
             [
-                'providerName' => 'Spotify',
+                'providerName' => 'spotify',
                 'logo' => Storage::url('spotify_logo.png'),
                 'alt' => 'spotify_logo',
                 'isConnected' => session('spotifyAccessToken'),
-                'href' => session('spotifyAccessToken') ? route('spotify.playlist') : route('spotify.authorize'),
+                'href' => session('spotifyAccessToken') ? 'transfer.target' : 'spotify.authorize',
             ],
             [
-                'providerName' => 'Youtube Music',
+                'providerName' => 'ytmusic',
                 'logo' => Storage::url('youtube_music_logo.png'),
                 'alt' => 'youtube_music_logo',
                 'isConnected' => session('ytMusicAccessToken'),
-                'href' => session('ytMusicRefreshToken') ? route('youtube.playlist') : route('youtube.authorize'),
+                'href' => session('ytMusicRefreshToken') ? 'transfer.target' : 'youtube.authorize',
             ],
         ];
 
         $header = 'Where would you like to transfer from?';
+
+        return inertia('Transfer/Show',
+            [
+                'buttonConfig' => $buttonConfig,
+                'header' => $header
+            ]);
+    }
+
+    public function target(Request $request): Response
+    {
+        $targetProvider = $request['provider'];
+
+        $buttonConfig = [
+            [
+                'providerName' => 'spotify',
+                'logo' => Storage::url('spotify_logo.png'),
+                'alt' => 'spotify_logo',
+                'isConnected' => session('spotifyAccessToken'),
+                'href' => session('spotifyAccessToken') ? '' : 'spotify.authorize',
+            ],
+            [
+                'providerName' => 'ytmusic',
+                'logo' => Storage::url('youtube_music_logo.png'),
+                'alt' => 'youtube_music_logo',
+                'isConnected' => session('ytMusicAccessToken'),
+                'href' => session('ytMusicRefreshToken') ? '' : 'youtube.authorize',
+            ],
+        ];
+
+        $header = 'Where would you like to transfer to?';
 
         return inertia('Transfer/Show',
             [
