c15fc0902628cd2694a2fe75bcc8789150ffbd35
David A. Adams <david.adams97@gmail.com>
2024-01-06 18:55:25 -0500

    feat: store last route and query params for redirecting

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -10,6 +10,9 @@ class TransferController extends Controller
 {
     public function source(): Response
     {
+        // Store the last route and query params since Spotify and YouTube authorization happens outside app domain
+        session(['lastRoute' => 'transfer.source']);
+
         $buttonConfig = [
             [
                 'providerName' => 'spotify',
@@ -39,6 +42,8 @@ class TransferController extends Controller
     public function target(Request $request): Response
     {
         $targetProvider = $request['source'];
+        // Store the last route and query params since Spotify and YouTube authorization happens outside app domain
+        session(['lastRoute' => 'transfer.target', 'queryParams' => 'source='.$sourceProvider]);
 
         $buttonConfig = [
             [
