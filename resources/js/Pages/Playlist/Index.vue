5e74a84e636f614fc5a623e86e819573bfd05ca4
David A. Adams <david@stemble.ca>
2024-01-05 19:04:06 -0500

    feat: create container for viewing playlist

---
+++ resources/js/Pages/Playlist/Index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex flex-col w-screen p-8 m-12">
+        <div
+            class="w-9/12 h-4/5 bg-gray-700/50 flex self-center justify-items-start rounded-2xl p-4 m-8"
+        >
+            <data-table
+                class="w-full"
+                no-data-text="No playlists found."
+                unnamed-entry="Unnamed playlist"
+                :headers="['Name of Playlist']"
+                :route-data="{
+                    name: 'spotify.playlist.list',
+                    params: 'id',
+                }"
+                :data-list="playlists"
+            >
+            </data-table>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import DataTable from "@/Components/DataTable.vue";
+
+type Provider = "spotify" | "ytmusic" | null;
+
+defineProps<{
+    provider: Provider;
+    playlists: [];
+}>();
+</script>
