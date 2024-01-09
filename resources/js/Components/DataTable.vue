4c6f87c7e34db6fb7be581f4f855e1417b2c4959
David A. Adams <david.adams97@gmail.com>
2024-01-09 16:20:16 -0500

    feat: display track information

--- resources/js/Components/DataTable.vue
+++ resources/js/Components/DataTable.vue
@@ -11,22 +11,43 @@
                 :key="entry.id"
                 class="hover:bg-gray-100 focus-within:bg-gray-100"
             >
-                <td v-for="col of entry.columns" :key="col" class="border-t">
-                    <Link
-                        v-if="routeData"
-                        class="flex items-center px-6 py-4 focus:text-indigo-500"
-                        :href="route(routeData.name, entry[routeData.params])"
-                    >
-                        {{ col || "Not Available" }}
-                    </Link>
+                <td
+                    v-for="(col, index) in entry.columns"
+                    :key="col"
+                    class="border-t"
+                >
+                    <!--                    <Link-->
+                    <!--                        v-if="routeData"-->
+                    <!--                        class="flex items-center px-6 py-4 focus:text-indigo-500"-->
+                    <!--                        :href="route(routeData.name, entry[routeData.params])"-->
+                    <!--                    >-->
+                    <!--                        {{ col || "Not Available" }}-->
+                    <!--                    </Link>-->
                     <span
-                        v-else
                         class="flex items-center px-6 py-4 focus:text-indigo-500"
                     >
+                        <input
+                            class="mr-2.5 rounded focus:ring-0 text-purple-900"
+                            v-if="index === 0"
+                            type="checkbox"
+                        />
+
                         {{ col || "Not Available" }}
                     </span>
+
+                    <div v-for="el in [2, 3]" class="flex pb-3 mr-3.5 ml-20">
+                        <input
+                            class="mr-2.5 rounded focus:ring-0 text-purple-900 flex self-center"
+                            type="checkbox"
+                        />
+                        <div class="flex flex-col">
+                            <span>{{ "Song Title" }}</span>
+                            <span class="text-sm">{{ "Album" }}</span>
+                        </div>
+                    </div>
                 </td>
             </tr>
+
             <tr v-if="dataList.length === 0">
                 <td class="px-6 py-4 border-t" colspan="4">
                     {{ noDataText }}
