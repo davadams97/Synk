78b3cc534f5c87a51948cb1345091485e5904870
David A. Adams <david.adams97@gmail.com>
2024-02-08 20:46:32 -0500

    style: cleanup styles

--- projects/synk/resources/js/Layouts/Layout.vue
+++ projects/synk/resources/js/Layouts/Layout.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="flex bg-gradient-to-r from-blue-950 from-75%% to-fuchsia-950 h-screen px-4 py-8 md:flex-1 md:p-12 md:overflow-y-auto"
+        class="flex flex-wrap bg-gradient-to-r from-blue-950 from-75%% to-fuchsia-950 h-screen px-4 py-8 md:p-12 overflow-y-auto"
     >
         <slot></slot>
     </div>
