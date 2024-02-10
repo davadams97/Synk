5157464bd0178f62986ac87f82cab0d610325d9a
David A. Adams <david.adams97@gmail.com>
2024-02-09 23:47:11 -0500

    feat: add navbar

--- projects/synk/resources/js/Layouts/Layout.vue
+++ projects/synk/resources/js/Layouts/Layout.vue
@@ -1,4 +1,7 @@
 <template>
+    <div class="bg-gradient-to-r from-blue-950 from-75%% to-fuchsia-950 h-10">
+        <a :href="route('home')" class="inline-block mt-4 ml-4 text-2xl white text-white tracking-widest">SYNK</a>
+    </div>
     <div
         class="flex flex-wrap bg-gradient-to-r from-blue-950 from-75%% to-fuchsia-950 h-screen px-4 py-8 md:p-12 overflow-y-auto"
     >
